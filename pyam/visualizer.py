eb33fc37be17b8bd1ee829c670e46ea23745f05f
David R. Pugh <drobert.pugh@gmail.com>
2015-07-27 10:25:37 +0100

    Refactored properties for computing distributions into a couple of methods.

--- pyam/visualizer.py
+++ pyam/visualizer.py
@@ -63,134 +63,28 @@ class Visualizer(pycollocation.Visualizer):
 
         return self.__solution
 
-    @property
-    def _soln_with_theta_frequency(self):
-        """Solution including the density for theta."""
+    def _theta_frequency(self):
+        """Compute the frequency (i.e, measure) of firm size."""
         tmp_df = self.solution.sort('theta', ascending=True, inplace=False)
-        tmp_df['theta_frequency'] = self.solver.problem.input1.evaluate_pdf(tmp_df.index.values) / tmp_df.theta
-        return tmp_df
-
-    @property
-    def factor_payment_1_cdf(self):
-        """
-        Cumulative distribution function (cdf) for factor payments to input 1.
-
-        :getter: Return the current cdf.
-        :type: pandas.Series
-
-        """
-        return self.compute_cdf(self.factor_payment_1_pdf)
-
-    @property
-    def factor_payment_1_pdf(self):
-        """
-        Probability density function (pdf) for factor payments to input 1.
-
-        :getter: Return the current pdf.
-        :type: pandas.Series
-
-        """
-        tmp_df = self._soln_with_theta_frequency.sort('factor_payment_1',
-                                                      ascending=True,
-                                                      inplace=False)
-        # frequency measured in number of units of input 1!
-        frequency = tmp_df.theta.values * tmp_df.theta_frequency.values
-        value = tmp_df.factor_payment_1.values
-        return pd.Series(frequency, index=value)
-
-    @property
-    def factor_payment_1_sf(self):
-        """
-        Survival function (sf) for factor payments to input 1.
-
-        :getter: Return the current sf.
-        :type: pandas.Series
-
-        """
-        return self.compute_sf(self.factor_payment_1_cdf)
-
-    @property
-    def factor_payment_2_cdf(self):
-        """
-        Cumulative distribution function (cdf) for factor payments to input 2.
-
-        :getter: Return the current cdf.
-        :type: pandas.Series
-
-        """
-        return self.compute_cdf(self.factor_payment_2_pdf)
-
-    @property
-    def factor_payment_2_pdf(self):
-        """
-        Probability density function (pdf) for factor payments to input 2.
-
-        :getter: Return the current pdf.
-        :type: pandas.Series
-
-        """
-        tmp_df = self._soln_with_theta_frequency.sort('factor_payment_2',
-                                                      ascending=True,
-                                                      inplace=False)
-        frequency = tmp_df.theta_frequency.values
-        value = tmp_df.factor_payment_2.values
-        return pd.Series(frequency, index=value)
-
-    @property
-    def factor_payment_2_sf(self):
-        """
-        Survival function (sf) for factor payments to input 2.
-
-        :getter: Return the current sf.
-        :type: pandas.Series
-
-        """
-        return self.compute_sf(self.factor_payment_2_cdf)
-
-    @property
-    def theta_cdf(self):
-        """
-        Cumulative distribution function (cdf) for theta.
-
-        :getter: Return the current cdf.
-        :type: pandas.Series
-
-        """
-        return self.compute_cdf(self.theta_pdf)
-
-    @property
-    def theta_pdf(self):
-        """
-        Probability density function (pdf) for theta.
-
-        :getter: Return the current pdf.
-        :type: pandas.Series
-
-        """
-        tmp_df = self._soln_with_theta_frequency
-        frequency = tmp_df.theta_frequency.values
-        value = tmp_df.theta.values
-        return pd.Series(frequency, index=value)
-
-    @property
-    def theta_sf(self):
-        """
-        Survival function (sf) for theta.
-
-        :getter: Return the current sf.
-        :type: pandas.Series
-
-        """
-        return self.compute_sf(self.theta_cdf)
+        input1_freq = self.solver.problem.input1.evaluate_pdf(tmp_df.index.values)
+        theta_frequency = input1_freq / tmp_df.theta
+        return theta_frequency
 
     @staticmethod
     def compute_cdf(pdf):
-        """Compute the cdf given a pdf."""
+        """Compute the cumulative distribution function (cdf) given a pdf."""
         cdf = pdf.cumsum() / pdf.sum()
         return cdf
 
     @staticmethod
     def compute_sf(cdf):
-        """Compute the sf given the cdf."""
+        """Compute the survival function (sf) given a cdf."""
         sf = 1 - cdf
         return sf
+
+    def compute_pdf(self, variable):
+        """Compute the probability density function (pdf) for some variable."""
+        tmp_df = self.solution.sort(variable, ascending=True, inplace=False)
+        tmp_df['theta_frequency'] = self._theta_frequency()
+        pdf = pd.Series(tmp_df.theta_frequency.values, index=tmp_df[variable].values)
+        return pdf
