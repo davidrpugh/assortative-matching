Improved initial guess algorithm; corrected bug in normalization of pdf; allowed for differing relative abundancies of inputs.

pyam/initial_guesses.py
@@ -28,7 +28,7 @@ class OrthogonalPolynomialInitialGuess(object):
         """
         self.solver = self._validate_solver(solver)
 
-    def _initial_mus(self, xs, exp):
+    def _initial_mus(self, xs, f, **params):
         """
         Return values for mu given some x values and an exponent.
 
@@ -50,14 +50,24 @@ class OrthogonalPolynomialInitialGuess(object):
         input1 = self.solver.problem.input1
         input2 = self.solver.problem.input2
 
-        slope = (input2.upper - input2.lower) / (input1.upper**exp - input1.lower**exp)
+        #slope = (input2.upper - input2.lower) / (input1.upper**exp - input1.lower**exp)
+        #slope = (input2.upper - input2.lower) / (np.exp(exp * input1.upper) - np.exp(exp * input1.lower))
+        slope = (input2.upper - input2.lower) / (f(input1.upper, **params) - f(input1.lower, **params))
+
         if self.solver.problem.assortativity == "positive":
-            intercept = input2.lower - slope * input1.lower**exp
+            #intercept = input2.lower - slope * input1.lower**exp
+            #intercept = input2.lower - slope * np.exp(exp * input1.lower)
+            intercept = input2.lower - slope * f(input1.lower, **params)
+
         else:
             slope = -slope
-            intercept = input2.upper - slope * input1.lower**exp
+            #intercept = input2.upper - slope * input1.lower**exp
+            intercept = input2.upper - slope * f(input1.lower, **params)
 
-        return intercept + slope * xs**exp
+
+       # return intercept + slope * xs**exp
+        #return intercept + slope * np.exp(exp * xs)
+        return intercept + slope * f(xs, **params)
 
     def _initial_guess_mu(self, xs, mus, kind, degree, domain):
         """Fit basis polynomial for mu of a certain kind, degree and domain."""
@@ -91,7 +101,7 @@ class OrthogonalPolynomialInitialGuess(object):
         else:
             return solver
 
-    def compute_initial_guess(self, kind, degrees, N=1000, exp=1.0):
+    def compute_initial_guess(self, kind, degrees, f, N=1000, **params):
         """
         Compute initial orthogonal polynomials.
 
@@ -109,7 +119,7 @@ class OrthogonalPolynomialInitialGuess(object):
         xs = np.linspace(domain[0], domain[1], N)
 
         # initial guess for mu is some power function
-        mus = self._initial_mus(xs, exp)
+        mus = self._initial_mus(xs, f, **params)
         initial_guess_mu = self._initial_guess_mu(xs, mus, kind, degrees['mu'], domain)
 
         # initial guess for theta depends on guess for mu

pyam/visualizer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 import pycollocation
@@ -73,7 +74,8 @@ class Visualizer(pycollocation.Visualizer):
     @staticmethod
     def compute_cdf(pdf):
         """Compute the cumulative distribution function (cdf) given a pdf."""
-        cdf = pdf.cumsum() / pdf.sum()
+        values = np.array([np.trapz(pdf.iloc[:x], pdf.index[:x]) for x in xrange(pdf.size)])
+        cdf = pd.Series(values, index=pdf.index.values)
         return cdf
 
     @staticmethod
@@ -82,9 +84,16 @@ class Visualizer(pycollocation.Visualizer):
         sf = 1 - cdf
         return sf
 
-    def compute_pdf(self, variable):
+    def compute_pdf(self, variable, normalize=True):
         """Compute the probability density function (pdf) for some variable."""
         tmp_df = self.solution.sort(variable, ascending=True, inplace=False)
         tmp_df['theta_frequency'] = self._theta_frequency()
-        pdf = pd.Series(tmp_df.theta_frequency.values, index=tmp_df[variable].values)
+
+        if normalize:
+            area = np.trapz(tmp_df.theta_frequency, tmp_df[variable])  # normalize by area!
+            density = tmp_df.theta_frequency / area
+            pdf = pd.Series(density.values, index=tmp_df[variable].values)
+        else:
+            pdf = pd.Series(tmp_df.theta_frequency.values, index=tmp_df[variable].values)
+
         return pdf